12488a9113a956d1b7b6513ec0f69c6d357cb995
itsmylifestyle <hahaii00@mail.ru>
2022-10-21 15:37:51 +0600

    Comm

--- Concantrationgame/ConcentrationGame.swift
+++ Concantrationgame/ConcentrationGame.swift
@@ -28,6 +28,7 @@ struct ConcentrationGame {
 //            return foundIndex
             
             //that means
+            //no
         }
         set {
             for index in cards.indices {
